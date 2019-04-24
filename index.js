e8bd183922d7c4e371fa6fad91fa96032253434b
Dil Sombayeva <dilyasom@gmail.com>
2019-04-24 10:18:17 -0700

    add index.js, server set up

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const server = express();
 const usersRouter = require('./data/routers/users');
+const postsRouter = require('./data/routers/posts');
+server.use(express.json());
 
 server.get('/', (req, res) => {
- res.sendStatus('Home page');
+ res.send('Home page');
 });
 server.use('/users', usersRouter);
+server.use('/posts', postsRouter);
 
 server.listen(8000, () => console.log('magic happens on port 8000'));
