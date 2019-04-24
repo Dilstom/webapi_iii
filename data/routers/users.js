30f4a4a886224c287e6670d4e5a905a323e1eae8
Dil Sombayeva <dilyasom@gmail.com>
2019-04-24 10:29:44 -0700

    users get /

--- data/routers/users.js
+++ data/routers/users.js
@@ -2,14 +2,16 @@ const express = require('express');
 const db = require('../helpers/userDb');
 const router = express.Router();
 
-router.get('/api/users', (req, res) => {
+router.get('/', (req, res) => {
  db
   .get()
   .then(users => {
    res.json(users);
   })
   .catch(err => {
-   return errorHelper(500, 'Server error', err);
+   res.status(500).json(err);
+  });
+});
   });
 });
 
