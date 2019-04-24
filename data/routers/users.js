c2781815c03e3d58696115a46480b76297e282fc
Dil Sombayeva <dilyasom@gmail.com>
2019-04-24 10:31:30 -0700

    get users posts

--- data/routers/users.js
+++ data/routers/users.js
@@ -12,6 +12,23 @@ router.get('/', (req, res) => {
    res.status(500).json(err);
   });
 });
+
+router.get('/posts/:id', (req, res) => {
+ const { id } = req.params;
+ db
+  .getUserPosts(id)
+  .then(userPosts => {
+   userPosts.length
+    ? res.json(userPosts)
+    : res
+       .status(404)
+       .json({ message: 'There are no any posts created by this user' });
+  })
+  .catch(err => {
+   res.status(500).json(err);
+  });
+});
+
   });
 });
 
